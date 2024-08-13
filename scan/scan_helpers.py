42673f32a2da0879de1885cb1655976cd1fafb86
Ben Eck <eck.mben@gmail.com>
2024-08-12 18:43:11 -0600

    initial commit of RCEW data analysis jupyter notebooks and scan package

---
+++ scan/scan_helpers.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from scipy.signal import savgol_filter
+import matplotlib.pyplot as plt
+
+def plot_scan_fp(df, measurement, **kwargs):
+    df.T.drop('Date/Time').plot(y=measurement, xlabel = 'Wavelength (nm)', ylabel= 'Absorbance', **kwargs)
+
+def import_scan_fp(path):
+    df = pd.read_csv(path, sep='\t', header=1).drop('Status_0', axis=1)
+    df['Date/Time'] = pd.to_datetime(df['Date/Time'])
+    #df.set_index('Date/Time', inplace=True)
+    return df
